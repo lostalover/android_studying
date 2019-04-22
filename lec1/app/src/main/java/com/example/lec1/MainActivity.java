8f12e24cdfec52ceb32bf4248b2431cc8ebb73e1
Harry <hdzzang93@gmail.com>
2019-04-22 19:36:14 +0900

    branch modified

--- lec1/app/src/main/java/com/example/lec1/MainActivity.java
+++ lec1/app/src/main/java/com/example/lec1/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //initialize
+        // initialize
         init();
         points = 0;
     }
