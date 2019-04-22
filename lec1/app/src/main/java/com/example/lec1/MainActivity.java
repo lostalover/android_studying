4b124de9456b15bd15f1100311ec3c515250a2ba
Harry <hdzzang93@gmail.com>
2019-04-22 19:43:11 +0900

    branchtest2 commit comment

--- lec1/app/src/main/java/com/example/lec1/MainActivity.java
+++ lec1/app/src/main/java/com/example/lec1/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // initialize
+        // push test for git branch
+
         init();
         points = 0;
     }
