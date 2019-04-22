79a8d82a638f7f9f58dad994eddb3798526f95b4
Harry <hdzzang93@gmail.com>
2019-04-22 19:22:10 +0900

    first commit

---
+++ lec1/app/src/main/java/com/example/lec1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.lec1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int num1;
+    private int num2;
+    private int points;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initialize
+        init();
+        points = 0;
+    }
+
+    public void init(){
+        Random randy = new Random();
+        Button left_button = (Button) findViewById(R.id.left_button);
+        Button right_button = (Button) findViewById(R.id.right_button);
+        TextView point_display = (TextView) findViewById(R.id.point_display);
+
+        num1 = randy.nextInt(100);
+        num2 = randy.nextInt(100);
+        while(true){
+            if(num2 != num1){
+                break;
+            }
+            num2 = randy.nextInt(100);
+        }
+        left_button.setText(String.valueOf(num1));
+        right_button.setText(String.valueOf(num2));
+        point_display.setText("point : " + points);
+    }
+
+    public void click_left(View view) {
+        if(num1 > num2){
+            points++;
+        }
+        else{
+            points--;
+        }
+        init();
+    }
+
+    public void click_right(View view) {
+        if(num2 > num1){
+            points++;
+        }
+        else{
+            points--;
+        }
+        init();
+    }
+}
