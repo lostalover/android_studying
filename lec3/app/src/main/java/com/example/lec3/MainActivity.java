56b52fa98a7e3c9fefbdef0f7929338eac297651
Harry <hdzzang93@gmail.com>
2019-05-09 23:39:24 +0900

    add list listener

--- lec3/app/src/main/java/com/example/lec3/MainActivity.java
+++ lec3/app/src/main/java/com/example/lec3/MainActivity.java
@@ -4,16 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String[] Words = {
+            "gyumin",
+            "soyuin",
+            "lee"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0;i<Words.length;i++){
+            list.add(Words[i]);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+
+        ListView listview = findViewById(R.id.my_list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("harry", "position = " + position);
+                Toast.makeText(MainActivity.this, "You clicked : " + Words[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void rabbitClick(View view) {
